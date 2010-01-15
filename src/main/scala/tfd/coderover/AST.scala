a26bffe24400d4ac3a08f03a2b1882680da62b5c
Tim Dalton <daltont@ociweb.com>
2010-01-15 10:45:49 -0600

    Using Scala 2.8 Beta RC 2 and AST changes based on compiler warnings

--- src/main/scala/tfd/coderover/AST.scala
+++ src/main/scala/tfd/coderover/AST.scala
@@ -34,25 +34,25 @@ sealed abstract class Expression()
     case class Negate(expression:IntExpression) extends IntExpression
     case class Mem(expression:IntExpression) extends IntExpression
 
- sealed abstract case class Mathematical(expressions:List[IntExpression]) extends IntExpression
-  case class Add(override val expressions:List[IntExpression]) extends Mathematical(expressions)
-  case class Subtract(override val expressions:List[IntExpression]) extends Mathematical(expressions)
-  case class Multiply(override val expressions:List[IntExpression]) extends Mathematical(expressions)
-  case class Divide(override val expressions:List[IntExpression]) extends Mathematical(expressions)
-  case class Modulus(override val expressions:List[IntExpression]) extends Mathematical(expressions)
+ sealed abstract class Mathematical(expressions:List[IntExpression]) extends IntExpression
+  case class Add(val expressions:List[IntExpression]) extends Mathematical(expressions)
+  case class Subtract(val expressions:List[IntExpression]) extends Mathematical(expressions)
+  case class Multiply(val expressions:List[IntExpression]) extends Mathematical(expressions)
+  case class Divide(val expressions:List[IntExpression]) extends Mathematical(expressions)
+  case class Modulus(val expressions:List[IntExpression]) extends Mathematical(expressions)
 
 sealed abstract class BooleanExpression() extends Expression
  case class Painted(x:IntExpression, y:IntExpression) extends BooleanExpression
  case class Adjacent(entity:String) extends BooleanExpression
  case class Not(booleanExpression:BooleanExpression) extends BooleanExpression
  case class Obstructed() extends BooleanExpression
- sealed abstract case class Logical() extends BooleanExpression
+ sealed abstract class Logical() extends BooleanExpression
   case class Or(val expressions:List[BooleanExpression])extends Logical
   case class And(val expressions:List[BooleanExpression]) extends Logical
- sealed abstract case class Comparison(left:IntExpression, right:IntExpression) extends BooleanExpression
-  case class LessThan(override val left:IntExpression, override val right:IntExpression) extends Comparison(left, right)
-  case class GreaterThan(override val left:IntExpression, override val right:IntExpression) extends Comparison(left, right)
-  case class Equal(override val left:IntExpression, override val right:IntExpression) extends Comparison(left, right)
-  case class GreaterThanOrEqual(override val left:IntExpression, override val right:IntExpression) extends Comparison(left, right)
-  case class LessThanOrEqual(override val left:IntExpression, override val right:IntExpression) extends Comparison(left, right)
-  case class NotEqual(override val left:IntExpression, override val right:IntExpression) extends Comparison(left, right)
+ sealed abstract class Comparison(left:IntExpression, right:IntExpression) extends BooleanExpression
+  case class LessThan(val left:IntExpression, val right:IntExpression) extends Comparison(left, right)
+  case class GreaterThan(val left:IntExpression, val right:IntExpression) extends Comparison(left, right)
+  case class Equal(val left:IntExpression, val right:IntExpression) extends Comparison(left, right)
+  case class GreaterThanOrEqual(val left:IntExpression, val right:IntExpression) extends Comparison(left, right)
+  case class LessThanOrEqual(val left:IntExpression, val right:IntExpression) extends Comparison(left, right)
+  case class NotEqual(val left:IntExpression, val right:IntExpression) extends Comparison(left, right)
