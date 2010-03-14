c38d35cb015038472643ef46bfcfcb9f0ab7f874
U-daltontk-PC\daltontk <daltontk@daltontk-PC.(none)>
2010-03-14 15:09:10 -0500

    Refactoring test

--- src/test/scala/tfd/coderover/LanguageParserTest.scala
+++ src/test/scala/tfd/coderover/LanguageParserTest.scala
@@ -10,7 +10,7 @@ class LanguageParserTest extends TestCase {
     code.foreach{ x => assertEquals(expectedAst, parseAll(parser, x).get) }
   }
 
-  private def assertProgramParsingProduces(expectedAst:List[Expression], code:String*) {
+  private def assertProgramParsingProduces(expectedAst:List[Any], code:String*) {
     code.foreach{ x => assertEquals(expectedAst, parse(x).get) }
   }
 
@@ -68,214 +68,213 @@ class LanguageParserTest extends TestCase {
   }
 
   def testSingleForward() {
-    assertEquals(List(Forward(Constant(1))), parse("FORWARD").get)
-    assertEquals(List(Forward(Constant(2))), parse("FORWARD 2").get)
+    assertProgramParsingProduces(List(Forward(Constant(1))), "FORWARD")
+    assertProgramParsingProduces(List(Forward(Constant(2))), "FORWARD 2")
   }
 
   def testRightLeft() {
-    assertEquals(List(TurnRight(), TurnLeft()), parse("""|RIGHT
-    										             |LEFT""".stripMargin).get)
+    assertProgramParsingProduces(List(TurnRight(), TurnLeft()), 
+      """|RIGHT
+    	   |LEFT""".stripMargin)
   }
 
   def testSimpleIfComparisons() {
-    assertEquals(List(If(Equal(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)), parse(
+    assertProgramParsingProduces(List(If(Equal(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)),
       """|
          |IF (1 = 2) {
          | LEFT
          | FORWARD
-         | }""".stripMargin).get)
-    assertEquals(List(If(LessThan(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)), parse(
+         | }""".stripMargin)
+    assertProgramParsingProduces(List(If(LessThan(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)), 
       """|
          |IF (1 < 2) {
          | LEFT
          | FORWARD
-         | }""".stripMargin).get)
-    assertEquals(List(If(GreaterThan(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)), parse(
+         | }""".stripMargin)
+    assertProgramParsingProduces(List(If(GreaterThan(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)),
       """|
          |IF (1 > 2) {
          | LEFT
          | FORWARD
-         | }""".stripMargin).get)
-    assertEquals(List(If(GreaterThanOrEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)), parse(
+         | }""".stripMargin)
+    assertProgramParsingProduces(List(If(GreaterThanOrEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)),
       """|
          |IF (1 >= 2) {
          | LEFT
          | FORWARD
-         | }""".stripMargin).get)
-    assertEquals(List(If(LessThanOrEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)), parse(
+         | }""".stripMargin)
+    assertProgramParsingProduces(List(If(LessThanOrEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)),
       """|
-         |IF (1 <= 2) {
+         |IF (1 <= 2) {                                                                                                               
          | LEFT
          | FORWARD
-         | }""".stripMargin).get)
-    assertEquals(List(If(NotEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)), parse(
+         | }""".stripMargin)
+    assertProgramParsingProduces(List(If(NotEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)),
       """|
          |IF (1 <> 2) {
          | LEFT
          | FORWARD
-         | }""".stripMargin).get)
+         | }""".stripMargin,
     // unnecessary params
-    assertEquals(List(If(NotEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), Nil)), parse(
       """|
          |IF ((1 <> 2)) {
          | LEFT
          | FORWARD
-         | }""".stripMargin).get)
+         | }""".stripMargin)
   }
 
   def testIfElse() {
-    assertEquals(List(If(NotEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), List(TurnRight()))), parse(
+    assertProgramParsingProduces(List(If(NotEqual(Constant(1), Constant(2)), List(TurnLeft(), Forward(Constant(1))), List(TurnRight()))), 
       """|
          |IF (1 <> 2) {
          | LEFT
          | FORWARD
          |} ELSE {
          | RIGHT
-         |}""".stripMargin).get)
+         |}""".stripMargin)
   }
 
   def testIfElseIf() {
-    assertEquals(List(
+    assertProgramParsingProduces(List(
       If(NotEqual(Constant(1), Constant(2)),
         List(TurnLeft(), Forward(Constant(1))),
         List(If(Equal(Constant(2), Constant(3)), List(TurnRight()), Nil))
-        )
-      ), parse(
-      """|
-         |IF (1 <> 2) {
-         | LEFT
-         | FORWARD
-         |} ELSE IF (2 = 3 ) {
-         | RIGHT
-         |}""".stripMargin).get)
+        )),
+        """|
+           |IF (1 <> 2) {
+           | LEFT
+           | FORWARD
+           |} ELSE IF (2 = 3 ) {
+           | RIGHT
+           |}""".stripMargin)
   }
 
   def testLogicalComparisonAndMathematical() {
-    assertEquals(List(
+    assertProgramParsingProduces(List(
       If(
         And(List(
           GreaterThan(Constant(1), Constant(-1)),
           NotEqual(Constant(-3), Add(List(Constant(2), Constant(-2)))))),
-        List(TurnLeft(), Forward(Add(List(Constant(1), Constant(2)))), TurnRight()), Nil)), parse(
+        List(TurnLeft(), Forward(Add(List(Constant(1), Constant(2)))), TurnRight()), Nil)), 
       """|
          |IF ((1 > -1) AND (-3 <> (2 + -2))) {
          | LEFT
          | FORWARD (1 + 2)
          | RIGHT
-         | }""".stripMargin).get)
+         | }""".stripMargin)
   }
 
   def testWhile() {
-    assertEquals(List(While(Equal(Constant(1), Constant(2)), List())), parse(
+    assertProgramParsingProduces(List(While(Equal(Constant(1), Constant(2)), List())),
       """|
          |WHILE (1 = 2) {
-         |}""".stripMargin).get)
-    assertEquals(List(While(Equal(Constant(1), Constant(2)), List())), parse(
+         |}""".stripMargin)
+    assertProgramParsingProduces(List(While(Equal(Constant(1), Constant(2)), List())), 
       """|
          |WHILE ((1 = 2)) {
-         |}""".stripMargin).get)
+         |}""".stripMargin)
   }
 
   def testPush() {
-    assertEquals(List(Push(Constant(1))), parse("""PUSH 1""").get)
-    assertEquals(List(Push(Add(List(Constant(1), Constant(2))))), parse("""PUSH (1+2)""").get)
-    assertEquals(List(Push(Subtract(List(Constant(8), GridX())))), parse("""PUSH (8 - X)""").get)
-    assertEquals(List(Push(Negate(Add(List(Constant(1), Constant(2)))))), parse("""PUSH -(1+2)""").get)
+    assertProgramParsingProduces(List(Push(Constant(1))), "PUSH 1")
+    assertProgramParsingProduces(List(Push(Add(List(Constant(1), Constant(2))))), "PUSH (1+2)")
+    assertProgramParsingProduces(List(Push(Subtract(List(Constant(8), GridX())))), "PUSH (8 - X)")
+    assertProgramParsingProduces(List(Push(Negate(Add(List(Constant(1), Constant(2)))))), "PUSH -(1+2)")
   }
 
   def testPop() {
-    assertEquals(List(Push(Constant(1)), Pop()), parse("""PUSH 1 POP""".stripMargin).get)
+    assertProgramParsingProduces(List(Push(Constant(1)), Pop()), "PUSH 1 POP")
   }
 
   def testReplace() {
-    assertEquals(List(Push(Constant(1)), Replace(Multiply(List(Top(), Constant(2))))),
-      parse("""|PUSH 1
-    			     |REPLACE (TOP * 2)""".stripMargin).get)
+    assertProgramParsingProduces(List(Push(Constant(1)), Replace(Multiply(List(Top(), Constant(2))))),
+      """|PUSH 1
+    	   |REPLACE (TOP * 2)""".stripMargin)
   }
 
   def testTop() {
-    assertEquals(List(If(LessThanOrEqual(Top(), Constant(-1)), List(), Nil)),
-      parse("""|IF (TOP <= -1) {
-		  			   |}""".stripMargin).get)
+    assertProgramParsingProduces(List(If(LessThanOrEqual(Top(), Constant(-1)), List(), Nil)),
+      """|IF (TOP <= -1) {
+		  	 |}""".stripMargin)
   }
 
   def testDepth() {
-    assertEquals(List(Push(Add(List(Depth(), Constant(1))))), parse("""PUSH (DEPTH + 1)""").get)
+    assertProgramParsingProduces(List(Push(Add(List(Depth(), Constant(1))))), "PUSH (DEPTH + 1)")
   }
 
   def testGridX() {
-    assertEquals(List(While(LessThan(GridX(), Constant(5)), List(Forward(Constant(1))))),
-      parse("""|WHILE (X < 5) {
-            	 | FORWARD 1
-		  			   |}""".stripMargin).get)
+    assertProgramParsingProduces(List(While(LessThan(GridX(), Constant(5)), List(Forward(Constant(1))))),
+      """|WHILE (X < 5) {
+         | FORWARD 1
+		     |}""".stripMargin)
   }
 
   def testGridY() {
-    assertEquals(List(While(GreaterThanOrEqual(GridY(), Constant(1)), List(Forward(Constant(1))))),
-      parse("""|WHILE (Y >= 1) {
-               | FORWARD 1
-		  			   |}""".stripMargin).get)
+    assertProgramParsingProduces(List(While(GreaterThanOrEqual(GridY(), Constant(1)), List(Forward(Constant(1))))),
+      """|WHILE (Y >= 1) {
+         | FORWARD 1
+		     |}""".stripMargin)
   }
 
   def testDeltaX() {
-    assertEquals(List(While(Equal(DeltaX(), Constant(1)), List(TurnRight()))),
-      parse("""|WHILE (DX = 1) {
-               | RIGHT
-		  			   |}""".stripMargin).get)
+    assertProgramParsingProduces(List(While(Equal(DeltaX(), Constant(1)), List(TurnRight()))),
+      """|WHILE (DX = 1) {
+         | RIGHT
+		  	 |}""".stripMargin)
   }
 
   def testDeltaY() {
-    assertEquals(List(While(Equal(DeltaY(), Constant(0)), List(TurnLeft()))),
-      parse("""|WHILE (DY = 0) {
-               | LEFT
-		  			   |}""".stripMargin).get)
+    assertProgramParsingProduces(List(While(Equal(DeltaY(), Constant(0)), List(TurnLeft()))),
+      """|WHILE (DY = 0) {
+         | LEFT
+		  	 |}""".stripMargin)
   }
 
   def testAbs() {
-    assertEquals(List(Push(Abs(Constant(-1)))), parse("PUSH ABS(-1)").get)
-    assertEquals(List(Push(Negate(Abs(Constant(-1))))), parse("PUSH -ABS(-1)").get)
-    assertEquals(List(Push(Abs(Negate(Add(List(Constant(1), Constant(2), Constant(3))))))), parse("PUSH ABS(-(1 + 2 + 3))").get)
+    assertProgramParsingProduces(List(Push(Abs(Constant(-1)))), "PUSH ABS(-1)")
+    assertProgramParsingProduces(List(Push(Negate(Abs(Constant(-1))))), "PUSH -ABS(-1)")
+    assertProgramParsingProduces(List(Push(Abs(Negate(Add(List(Constant(1), Constant(2), Constant(3))))))), "PUSH ABS(-(1 + 2 + 3))")
   }
 
   def testMax() {
-    assertEquals(List(Push(Max(Constant(-1), Constant(2)))), parse("PUSH MAX(-1, 2)").get)
+    assertProgramParsingProduces(List(Push(Max(Constant(-1), Constant(2)))), "PUSH MAX(-1, 2)")
   }
 
   def testMin() {
-    assertEquals(List(Push(Min(Constant(-1), Constant(2)))), parse("PUSH MIN(-1, 2)").get)
+    assertProgramParsingProduces(List(Push(Min(Constant(-1), Constant(2)))), "PUSH MIN(-1, 2)")
   }
 
   def testPaint() {
-    assertEquals(List(Paint()), parse("PAINT").get)
+    assertProgramParsingProduces(List(Paint()), "PAINT")
   }
 
   def testPainted() {
-    assertEquals(List((While(Painted(Constant(1), Constant(2)), List(Forward(Constant(1)))))),
-      parse("""WHILE (PAINTED(1,2)) { FORWARD }""").get)
-    assertEquals(List((While(Painted(Constant(1), Constant(2)), List(Forward(Constant(1)))))),
-      parse("""WHILE PAINTED(1,2) { FORWARD }""").get)
-    assertEquals(List((While(Painted(Constant(1), Constant(2)), List(Forward(Constant(1)))))),
-      parse("""WHILE ((PAINTED(1,2))) { FORWARD }""").get) 
+    assertProgramParsingProduces(List((While(Painted(Constant(1), Constant(2)), List(Forward(Constant(1)))))),
+      "WHILE (PAINTED(1,2)) { FORWARD }")
+    assertProgramParsingProduces(List((While(Painted(Constant(1), Constant(2)), List(Forward(Constant(1)))))),
+      "WHILE PAINTED(1,2) { FORWARD }")
+    assertProgramParsingProduces(List((While(Painted(Constant(1), Constant(2)), List(Forward(Constant(1)))))),
+      "WHILE ((PAINTED(1,2))) { FORWARD }")
 
   }
 
   def testNot() {
-    assertEquals(List((While(Not(Painted(Constant(1), Constant(2))), List(Forward(Constant(1)))))),
-      parse("""WHILE (NOT(PAINTED(1,2))) { FORWARD }""").get)
+    assertProgramParsingProduces(List((While(Not(Painted(Constant(1), Constant(2))), List(Forward(Constant(1)))))),
+      "WHILE (NOT(PAINTED(1,2))) { FORWARD }")
   }
 
   def testAdjacent() {
-    assertEquals(List(If(Adjacent("MINE"), List(TurnRight()), Nil)), parse("IF (ADJACENT(MINE)) { RIGHT }").get)
+    assertProgramParsingProduces(List(If(Adjacent("MINE"), List(TurnRight()), Nil)), "IF (ADJACENT(MINE)) { RIGHT }")
   }
 
   def testDistances() {
-    assertEquals(List(Push(DistanceX("FLAG"))), parse("PUSH DISTANCEX(FLAG)").get)
-    assertEquals(List(Push(DistanceY("ROCK"))), parse("PUSH DISTANCEY(ROCK)").get)
-    assertEquals(List(Push(Min(DistanceX("FLAG"), DistanceY("FLAG")))), parse("PUSH MIN(DISTANCEX(FLAG), DISTANCEY(FLAG))").get)
+    assertProgramParsingProduces(List(Push(DistanceX("FLAG"))), "PUSH DISTANCEX(FLAG)")
+    assertProgramParsingProduces(List(Push(DistanceY("ROCK"))), "PUSH DISTANCEY(ROCK)")
+    assertProgramParsingProduces(List(Push(Min(DistanceX("FLAG"), DistanceY("FLAG")))), "PUSH MIN(DISTANCEX(FLAG), DISTANCEY(FLAG))")
   }
 
   def testDef() {
-    assertEquals(List(Def("FUNC", List(TurnLeft(), TurnLeft()))), parse("DEF FUNC { LEFT LEFT }").get)
+    assertProgramParsingProduces(List(Def("FUNC", List(TurnLeft(), TurnLeft()))), "DEF FUNC { LEFT LEFT }")
   }
 
   def testCall() {
@@ -285,51 +284,51 @@ class LanguageParserTest extends TestCase {
       "CALL FUNC(42)" -> List(Call("FUNC", List(Constant(42))))
     ).map {
        case (code:String, ast:List[Instruction]) =>
-      assertEquals(ast, parse(code).get)                          
+      assertProgramParsingProduces(ast, code)                          
     }
-    assertEquals(List(Call("FUNC", List(Constant(42), Add(List(Top(), Constant(28)))))), parse("CALL FUNC(42,TOP + 28)").get)
-    assertEquals(List(Call("FUNC", List(Constant(42), Add(List(Top(), Constant(28)))))), parse("CALL FUNC(42,(TOP + 28))").get)
-    assertEquals(List(Call("FUNC", List(Constant(42), Negate(Add(List(Top(), Constant(28))))))), parse("CALL FUNC(42,-(TOP + 28))").get)
-    assertEquals(List(Call("FUNC", List(Constant(42), Add(List(Negate(Top()), Constant(28)))))), parse("CALL FUNC(42,-TOP + 28)").get)
+    assertProgramParsingProduces(List(Call("FUNC", List(Constant(42), Add(List(Top(), Constant(28)))))), "CALL FUNC(42,TOP + 28)")
+    assertProgramParsingProduces(List(Call("FUNC", List(Constant(42), Add(List(Top(), Constant(28)))))), "CALL FUNC(42,(TOP + 28))")
+    assertProgramParsingProduces(List(Call("FUNC", List(Constant(42), Negate(Add(List(Top(), Constant(28))))))), "CALL FUNC(42,-(TOP + 28))")
+    assertProgramParsingProduces(List(Call("FUNC", List(Constant(42), Add(List(Negate(Top()), Constant(28)))))), "CALL FUNC(42,-TOP + 28)")
   }
 
   def testPrint() {
-    assertEquals(List(Print(List(StringConstant("X = "), GridX()))), parse("""PRINT "X = " + X""").get)
+    assertProgramParsingProduces(List(Print(List(StringConstant("X = "), GridX()))), """PRINT "X = " + X""")
   }
 
   def testStore() {
-    assertEquals(List(Store(Constant(1), GridX())), parse("""STORE (1, X)""").get)
+    assertProgramParsingProduces(List(Store(Constant(1), GridX())), """STORE (1, X)""")
   }
 
   def testMem() {
-    assertEquals(List(Push(Mem(Constant(1)))), parse("""PUSH MEM(1)""").get)
+    assertProgramParsingProduces(List(Push(Mem(Constant(1)))), """PUSH MEM(1)""")
   }
 
   def testComments() {
-    assertEquals(List(While(Equal(DeltaY(), Constant(0)), List(TurnLeft()))),
-      parse("""|WHILE (DY = 0) { // comment
-               | LEFT
-               |}//comment""".stripMargin).get)
-    assertEquals(List(While(Equal(DeltaY(), Constant(0)), List(TurnLeft()))),
-      parse("""|WHILE /* multi-line
-               | comment */ (DY = 0) {
-               | LEFT  /* Comment */
-		  			   |}""".stripMargin).get)
-    assertEquals(List(While(Equal(DeltaY(), Constant(0)), List(TurnLeft()))),
-      parse("""|WHILE (DY = 0) { // Comment
-               | /* Comment */ LEFT
-		  			   |}""".stripMargin).get)
+    assertProgramParsingProduces(List(While(Equal(DeltaY(), Constant(0)), List(TurnLeft()))),
+      """|WHILE (DY = 0) { // comment
+         | LEFT
+         |}//comment""".stripMargin)
+    assertProgramParsingProduces(List(While(Equal(DeltaY(), Constant(0)), List(TurnLeft()))),
+      """|WHILE /* multi-line
+         | comment */ (DY = 0) {
+         | LEFT  /* Comment */
+		     |}""".stripMargin)
+    assertProgramParsingProduces(List(While(Equal(DeltaY(), Constant(0)), List(TurnLeft()))),
+      """|WHILE (DY = 0) { // Comment
+         | /* Comment */ LEFT
+		     |}""".stripMargin)
   }
 
   def testObstructed() {
-    assertEquals(List(If(Obstructed(Add(List(GridX(), DeltaX())), Add(List(GridY(), DeltaY()))), List(TurnLeft()), Nil)), parse("IF (OBSTRUCTED(X+DX,Y+DY)) { LEFT }").get)
+    assertProgramParsingProduces(List(If(Obstructed(Add(List(GridX(), DeltaX())), Add(List(GridY(), DeltaY()))), List(TurnLeft()), Nil)), "IF (OBSTRUCTED(X+DX,Y+DY)) { LEFT }")
   }
 
   def testNegativeExpression() {
-    assertEquals(List(Print(List(StringConstant("-DX = "), Negate(DeltaX())))), parse("""PRINT "-DX = " + -DX""").get)
+    assertProgramParsingProduces(List(Print(List(StringConstant("-DX = "), Negate(DeltaX())))), """PRINT "-DX = " + -DX""")
   }
 
   def testParameters() {
-    assertEquals(List(Forward(Param(1))), parse("""FORWARD :1""").get)
+    assertProgramParsingProduces(List(Forward(Param(1))), """FORWARD :1""")
   }
 }
